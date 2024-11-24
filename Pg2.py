9d927deb18d4864117a73cdf2db7c0a79901645b
JobbyAnil <149676865+JobbyAnil@users.noreply.github.com>
2024-11-24 19:27:58 +0530

    Create Pg2.py

---
+++ Pg2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+size = int(input("Enter the size of the square matrix: "))
+
+matrix = []
+
+print("Enter each row of the matrix, with elements separated by spaces:")
+
+for i in range(size):
+
+ row = list(map(float, input().split()))
+
+ matrix.append(row)
+
+trans=np.transpose(matrix)
+
+print(trans)
