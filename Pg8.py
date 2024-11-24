6099080c9f5b1acb838b19cdf4a7bc1c63256e79
JobbyAnil <149676865+JobbyAnil@users.noreply.github.com>
2024-11-24 19:31:09 +0530

    Create Pg8.py

---
+++ Pg8.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+x=np.array([88,92,18,89,100,80,60,100,80,34])
+
+y=np.array([35,79,79,48,100,88,32,45,20,30])
+
+plt.title('Scatter Plot: Mathematics vs Science')
+
+plt.grid(True)
+
+plt.scatter(x,y)
+
+plt.xlabel("Maths mark")
+
+plt.ylabel("Science mark")
+
+plt.title("MARKS")
+
+plt.show()
